7ad9925b910f7a6ea794bbc6b921ac90fd19c2ba
zCoder0 <131846622+zCoder0@users.noreply.github.com>
2025-01-01 15:09:19 +0530

    Update signup.py

--- signup.py
+++ signup.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from databasecon import connection
 def signup_page():
     st.title("Signup Page")
     
@@ -10,18 +9,12 @@ def signup_page():
     new_password = st.text_input("New Password", type="password")
     cn_password = st.text_input("Confirm Password", type="password")
     signup_button = st.button("Signup")
-    sql ="insert into user(username,email,mobile,password) values (%s,%s,%s,%s)"
-    val =(new_username,email,mobile,new_password)
     if signup_button:
         # Replace with your signup logic (e.g., save to a database)
         if cn_password == new_password:
             # Save to database
-            conn,cur = connection()
-            cur.execute(sql,val)
-            conn.commit()
-            if cur.rowcount >= 1:
-                st.success("Account created successfully! Please log in.")
-            else:
-                st.error("Failed to create account")
+           
+            st.success("Account created successfully! Please log in.")
+
         else:
             st.error("Missmatch password .")
